58e3d4e6a6bb41ed094fc60fbfd16d69e5791a02
TonnyHatter <steven_anthony_hall@yahoo.com>
2025-08-03 23:11:57 -0700

    B: Starting to get going.

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,4 +1,6 @@
 import os
+import subprocess
+from config import MAX_CHARACTERS
 
 def get_files_info(working_directory, directory="."):
     try:
@@ -23,3 +25,63 @@ def get_files_info(working_directory, directory="."):
         return f'Error: An OS error occurred - {str(e)}'
     except Exception as e:
         return f'Error: An unexpected error occurred - {str(e)}'
+
+def get_file_content(working_directory, file_path):
+    try:
+        absolute_path = os.path.abspath(os.path.join(working_directory, file_path))
+        if not absolute_path.startswith(os.path.abspath(working_directory)):
+            return f'Error: Cannot list "{file_path}" as it is outside the permitted working directory'
+        if not os.path.isfile(absolute_path):
+            return f'Error: File not found or is not a regular file: "{file_path}"'
+        with open(absolute_path, "r") as f:
+            file_contents_string = f.read(MAX_CHARACTERS)
+            if len(file_contents_string) == MAX_CHARACTERS:
+                file_contents_string += '[...File "{file_path}" truncated at 10000 characters]'
+        return file_contents_string
+    except PermissionError as e:
+        return f"Error: permission denied for '{file_path}' - {str(e)}"
+    except FileNotFoundError as e:
+        return f"Error: File '{file_path}' not found - {str(e)}"
+    except OSError as e:
+        return f"Error: An OS Error occurred - {str(e)}"
+    except Exception as e:
+        return f"Error: An unexpected error occurred - {str(e)}"
+
+def write_file(working_directory, file_path, content):
+    try:
+        absolute_path = os.path.abspath(os.path.join(working_directory, file_path))
+        if not absolute_path.startswith(os.path.abspath(working_directory)):
+            return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+        directory_path = os.path.dirname(absolute_path)
+        if not os.path.exists(directory_path):
+            os.path.makedirs(directory_path)
+        with open(absolute_path, "w") as f:
+            f.write(content)
+        return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
+    except PermissionError as e:
+        return f"Error: permission denied for '{file_path}' - {str(e)}"
+    except FileNotFoundError as e:
+        return f"Error: File '{file_path}' not found - {str(e)}"
+    except OSError as e:
+        return f"Error: An OS Error occurred - {str(e)}"
+    except Exception as e:
+        return f"Error: An unexpected error occurred - {str(e)}"
+
+def run_python_file(working_directory, file_path, args=[]):
+    try:
+        absolute_path = os.path.abspath(os.path.join(working_directory, file_path))
+        if not absolute_path.startswith(os.path.abspath(working_directory)):
+            return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+        if not os.path.exists(absolute_path):
+            return f'Error: File "{file_path}" not found.'
+        if not file_path.endswith(".py"):
+            return f'Error: "{file_path}" is not a Python file.'
+        cmd = ["python3", file_path] + args
+        result = subprocess.run(cmd, capture_output=True, text=True, cwd=working_directory, timeout=30)
+        if result.returncode != 0:
+            return f"STDOUT: {result.stdout} \nSTDERR: {result.stderr} \nProcess exited with code {result.returncode}"
+        if not result.stdout and not result.stderr:
+            return f"No output produced."
+        return f"STDOUT: {result.stdout} \nSTDERR: {result.stderr}"
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
--- tests.py
+++ tests.py
@@ -1,9 +1,12 @@
 #Unit tests for the get_files_info function
 import os
-from functions.get_files_info import get_files_info
+from functions.get_files_info import get_file_content
+from functions.get_files_info import write_file
+from functions.get_files_info import run_python_file
 
 if __name__ == "__main__":
-    print(f"Result for current directory \n{get_files_info('calculator', '.')}")
-    print(f"Result for 'pkg' directory \n{get_files_info('calculator', 'pkg')}")
-    print(f"Result for '/bin' directory \n{get_files_info('calculator', '/bin')}")
-    print(f"Result for '../' directory \n{get_files_info('calculator', '../')}")
+    print(run_python_file("calculator", "main.py"))
+    print(run_python_file("calculator", "main.py", ["3 + 5"]))
+    print(run_python_file("calculator", "tests.py"))
+    print(run_python_file("calculator", "../main.py"))
+    print(run_python_file("calculator", "nonexistent.py"))
